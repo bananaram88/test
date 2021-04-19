7b823e854be8e4f1031573bf3b67b13dc1b02c46
bananaram88 <yoeramlin@gmail.com>
2021-04-19 15:44:17 -0700

    second iteration

--- testGit.c
+++ testGit.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h >
 
-int main(void){
-	printf("hello world");
+int main(void)
+{
+	int choice, i;
+
+	printf("Pick a number between 1 and 100: ");
+	scanf("%d", choice);
+
+	for(i=0; i<iterations; i++){
+
+	
+		printf("you are awesome!\n");
+	}
 
 	return EXIT_SUCCESS;
 }
